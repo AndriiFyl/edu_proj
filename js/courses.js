81c4715b1dc2b4ea8a255a68bc4ca5a96f024a00
Fyl Andrii <fil.andrey91@gmail.com>
2023-04-15 12:50:37 +0300

    changes

--- js/courses.js
+++ js/courses.js
@@ -3,98 +3,98 @@ export const arrayOfAllCourses = [
     name: "JavaScript",
     category: "програмування",
     level: "початковий",
-    img: "../images/js.jpg",
+    img: "./images/js.jpg",
     id: 1
   },
   {
     name: "English",
     category: "вивчення мови",
     level: "початковий",
-    img: "../images/English.jpg",
+    img: "./images/English.jpg",
     id: 2,
   },
   {
     name: "English",
     category: "вивчення мови",
     level: "середній",
-    img: "../images/English.jpg",
+    img: "./images/English.jpg",
     id: 3
   },
   {
     name: "Java",
     category: "програмування",
     level: "середній",
-    img: "../images/java.jpg",
+    img: "./images/java.jpg",
     id: 4
   },
   {
     name: "Figma",
     category: "дизайн",
     level: "середній",
-    img: "../images/figma.jpg",
+    img: "./images/figma.jpg",
     id: 5
   },
   {
     name: "Photoshop",
     category: "дизайн",
     level: "високий",
-    img: "../images/ps.jpg",
+    img: "./images/ps.jpg",
     id: 6
   },
   {
     name: "JavaScript",
     category: "програмування",
     level: "високий",
-    img: "../images/js.jpg",
+    img: "./images/js.jpg",
     id: 7
   },
   {
     name: "English",
     category: "вивчення мови",
     level: "високий",
-    img: "../images/English.jpg",
+    img: "./images/English.jpg",
     id: 8,
   },
   {
     name: "Java",
     category: "програмування",
     level: "високий",
-    img: "../images/java.jpg",
+    img: "./images/java.jpg",
     id: 9
   },
     {
     name: "Figma",
     category: "дизайн",
     level: "початковий",
-    img: "../images/figma.jpg",
+    img: "./images/figma.jpg",
     id: 10
   },
     {
     name: "Photoshop",
     category: "дизайн",
     level: "середній",
-    img: "../images/ps.jpg",
+    img: "./images/ps.jpg",
     id: 11
   },
     {
     name: "Photoshop",
     category: "дизайн",
     level: "початковий",
-    img: "../images/ps.jpg",
+    img: "./images/ps.jpg",
     id: 12
   },
     {
     name: "3D Max",
     category: "дизайн",
     level: "початковий",
-    img: "../images/3DMax.jpeg",
+    img: "./images/3DMax.jpeg",
     id: 13
   },
     {
     name: "Phyton",
     category: "програмування",
     level: "початковий",
-    img: "../images/phyton.jpg",
+    img: "./images/phyton.jpg",
     id: 14
   },
 ]
